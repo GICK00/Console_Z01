708dcffc3087c2bdf71891cc1c651326d8ea4fab
GICK00 <71695624+GICK00@users.noreply.github.com>
2021-11-29 09:59:45 +0300

    Add files via upload

---
+++ Console_Z01/Console_Z01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Console_Z01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repit = true;
+            while(repit == true)
+            {
+                repit = false;
+                try
+                {
+                    Console.WriteLine("| Введите объем куба.");
+                    Console.Write("| V = ");
+                    double V = Convert.ToDouble(Console.ReadLine());
+                    double result = Math.Pow(V, 1 / 3.0);
+                    Console.WriteLine("| Ребро куба = " + result);
+
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Попробовать снова? Да / Нет");
+                    Console.Write("| : ");
+                    string repitTxT = Convert.ToString(Console.ReadLine());
+
+                    if (repitTxT == "Да")
+                    {
+                        repit = true;
+                        Console.WriteLine("|---------------------------");
+                    }
+                    else
+                        repit = false;
+                } catch (Exception)
+                {
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Некорректный ввод данных!");
+                    Console.WriteLine("| Попробовать снова? Да / Нет");
+                    Console.Write("| : ");
+                    string repitTxT = Convert.ToString(Console.ReadLine());
+                    try
+                    {
+                        if (repitTxT == "Да")
+                        {
+                            repit = true;
+                            Console.WriteLine("|---------------------------");
+                        }
+                        else
+                        {
+                            if (repitTxT == "Нет")
+                            {
+                                repit = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("|---------------------------");
+                                Console.WriteLine("| Некорректный ввод данных!");
+                                repit = false;
+                            }
+                        } 
+                    } catch (Exception)
+                    {
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Некорректный ввод данных!");
+                        repit = false;
+                    }   
+                }
+            }
+        }
+    }
+}
---
+++ Console_Z01_2/Console_Z01_2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Console_Z01_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("| Проверка суммы цифр числа на не четность.");
+            bool repit = true;
+            try
+            {
+                while (repit == true)
+                {
+                    Console.Write("| Введите двузначное число: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+
+                    if (n.ToString().Length == 2)
+                    {
+                        int sum = 0;
+                        while (n > 0)
+                        {
+                            sum = sum + n % 10;
+                            n = n / 10;
+                        }
+
+                        if (sum % 2 != 0)
+                        {
+                            Console.WriteLine("| Сумма цифр {0} не четное", sum);
+                        }
+                        else
+                        {
+                            Console.WriteLine("| Сумма цифр {0} четное", sum);
+                        }
+
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Попробовать снова? Да / Нет");
+                        Console.Write("| : ");
+                        string repitTxT = Convert.ToString(Console.ReadLine());
+
+                        if (repitTxT == "Да")
+                        {
+                            repit = true;
+                            Console.WriteLine("|---------------------------");
+                        }
+                        else
+                            repit = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("| Введите двузначное число!");
+                        Console.WriteLine("|---------------------------");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("|---------------------------");
+                Console.WriteLine("| Некорректный ввод данных!");
+                Console.WriteLine("| Попробовать снова? Да / Нет");
+                Console.Write("| : ");
+                string repitTxT = Convert.ToString(Console.ReadLine());
+                try
+                {
+                    if (repitTxT == "Да")
+                    {
+                        repit = true;
+                        Console.WriteLine("|---------------------------");
+                    }
+                    else
+                    {
+                        if (repitTxT == "Нет")
+                        {
+                            repit = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("|---------------------------");
+                            Console.WriteLine("| Некорректный ввод данных!");
+                            repit = false;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Некорректный ввод данных!");
+                    repit = false;
+                }
+            }
+        }
+    }
+}
